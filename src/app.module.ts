f85293f5e41c2289712678a5369986103f955968
HuitziDomingo <false>
2024-01-15 21:11:38 -0600

    primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ChatModule } from './chat/chat.module';
+
+
+@Module({
+  imports: [ChatModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,29 @@
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets'
+import { OnModuleInit } from '@nestjs/common'
+import { Server, Socket } from 'socket.io'
+
+import { ChatService } from './chat.service'
+
+@WebSocketGateway()
+export class ChatGateway implements OnModuleInit {
+
+  @WebSocketServer()
+  public server: Server
+
+  constructor(private readonly chatService: ChatService) { }
+
+
+  onModuleInit() {
+    this.server.on('connection', (socket: Socket) => {
+      console.log('cliente conectado')
+
+      socket.on('disconnect', () => {
+        console.log('se desconecto')
+      })
+    })
+  }
+
+
+
+
+}
